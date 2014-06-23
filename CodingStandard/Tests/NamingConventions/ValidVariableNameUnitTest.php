fa4996ccee015713e759e34536124bdf1bf21a5a
Alexander Obuhovich <aik.bold@gmail.com>
2014-06-23 14:34:23 +0300

    Adding sniff to validate proper variable naming

---
+++ CodingStandard/Tests/NamingConventions/ValidVariableNameUnitTest.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Unit test class for the ValidVariableName sniff.
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Greg Sherwood <gsherwood@squiz.net>
+ * @author   Marc McIntyre <mmcintyre@squiz.net>
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+
+/**
+ * Unit test class for the ValidVariableName sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Greg Sherwood <gsherwood@squiz.net>
+ * @author   Marc McIntyre <mmcintyre@squiz.net>
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+class CodingStandard_Tests_NamingConventions_ValidVariableNameUnitTest extends AbstractSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @param string $testFile Name of the file with test data.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList($testFile)
+    {
+        return array(
+                2   => 1,
+                5   => 1,
+                10  => 1,
+                12  => 1,
+                15  => 1,
+                17  => 1,
+                20  => 1,
+                22  => 1,
+                25  => 1,
+                27  => 1,
+                30  => 1,
+                33  => 1,
+                35  => 1,
+                37  => 1,
+                40  => 1,
+                42  => 1,
+                45  => 1,
+                54  => 1,
+                59  => 1,
+                63  => 1,
+                64  => 1,
+                65  => 1,
+                68  => 1,
+                71  => 1,
+                79  => 1,
+                80  => 1,
+                81  => 1,
+                82  => 1,
+                84  => 1,
+                109 => 1,
+                110 => 1,
+                111 => 1,
+                119 => 1,
+                123 => 2,
+                124 => 1,
+                133 => 1,
+                137 => 2,
+                138 => 1,
+               );
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @param string $testFile Name of the file with test data.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList($testFile)
+    {
+        return array();
+
+    }//end getWarningList()
+
+
+}//end class
+
+?>
