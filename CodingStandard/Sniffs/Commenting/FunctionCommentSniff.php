14f4ec80e2ea84c7b87c2f038f48b31d4482fd5f
Greg Sherwood <gsherwood@squiz.net>
2014-08-15 11:10:49 +1000

    Mute unused error instead of overriding process()
    Instead of overriding the main process() method completley, we can just perform some additional checks and then use the rulset.xml to mute the ShortNoCaptial error that we don't want. Instead, we do our own short comment checks and use different codes so they are not confused.
    
    This sniff isn't 2.x ready yet, but hopefully this will make it easier to become ready.

--- CodingStandard/Sniffs/Commenting/FunctionCommentSniff.php
+++ CodingStandard/Sniffs/Commenting/FunctionCommentSniff.php
@@ -33,46 +33,6 @@ if (class_exists('Squiz_Sniffs_Commenting_FunctionCommentSniff', true) === false
 class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs_Commenting_FunctionCommentSniff
 {
 
-    /**
-     * Reflection of parent sniff private property.
-     *
-     * @var ReflectionProperty
-     */
-    private $_functionTokenReflection;
-
-    /**
-     * Reflection of parent sniff private property.
-     *
-     * @var ReflectionProperty
-     */
-    private $_classTokenReflection;
-
-    /**
-     * Reflection of parent sniff private property.
-     *
-     * @var ReflectionProperty
-     */
-    private $_methodNameReflection;
-
-
-    /**
-     * Stores class reflection.
-     */
-    public function __construct()
-    {
-        $parentClass = 'Squiz_Sniffs_Commenting_FunctionCommentSniff';
-
-        $this->_functionTokenReflection = new ReflectionProperty($parentClass, '_functionToken');
-        $this->_functionTokenReflection->setAccessible(true);
-
-        $this->_classTokenReflection = new ReflectionProperty($parentClass, '_classToken');
-        $this->_classTokenReflection->setAccessible(true);
-
-        $this->_methodNameReflection = new ReflectionProperty($parentClass, '_methodName');
-        $this->_methodNameReflection->setAccessible(true);
-
-    }//end __construct()
-
 
     /**
      * Processes this test, when one of its tokens is encountered.
@@ -85,7 +45,7 @@ class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs
      */
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
-        $this->currentFile = $phpcsFile;
+        parent::process($phpcsFile, $stackPtr);
 
         $tokens = $phpcsFile->getTokens();
 
@@ -98,7 +58,6 @@ class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs
                 );
 
         $commentEnd = $phpcsFile->findPrevious($find, ($stackPtr - 1));
-
         if ($commentEnd === false) {
             return;
         }
@@ -107,29 +66,12 @@ class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs
         // function has no doc comment.
         $code = $tokens[$commentEnd]['code'];
 
-        if ($code === T_COMMENT) {
-            // The function might actually be missing a comment, and this last comment
-            // found is just commenting a bit of code on a line. So if it is not the
-            // only thing on the line, assume we found nothing.
-            $prevContent = $phpcsFile->findPrevious(PHP_CodeSniffer_Tokens::$emptyTokens, $commentEnd);
-            if ($tokens[$commentEnd]['line'] === $tokens[$prevContent]['line']) {
-                $error = 'Missing function doc comment';
-                $phpcsFile->addError($error, $stackPtr, 'Missing');
-            } else {
-                $error = 'You must use "/**" style comments for a function comment';
-                $phpcsFile->addError($error, $stackPtr, 'WrongStyle');
-            }
-
+        if ($code === T_COMMENT
+            || $code !== T_DOC_COMMENT
+            || trim($tokens[$commentEnd]['content']) !== '*/'
+        ) {
             return;
-        } else if ($code !== T_DOC_COMMENT) {
-            $error = 'Missing function doc comment';
-            $phpcsFile->addError($error, $stackPtr, 'Missing');
-            return;
-        } else if (trim($tokens[$commentEnd]['content']) !== '*/') {
-            $error = 'You must use "*/" to end a function comment; found "%s"';
-            $phpcsFile->addError($error, $commentEnd, 'WrongEnd', array(trim($tokens[$commentEnd]['content'])));
-            return;
-        }//end if
+        }
 
         // If there is any code between the function keyword and the doc block
         // then the doc block is not for us.
@@ -140,16 +82,13 @@ class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs
         $ignore[]  = T_FINAL;
         $prevToken = $phpcsFile->findPrevious($ignore, ($stackPtr - 1), null, true);
         if ($prevToken !== $commentEnd) {
-            $phpcsFile->addError('Missing function doc comment', $stackPtr, 'Missing');
             return;
         }
 
-        $this->_functionTokenReflection->setValue($this, $stackPtr);
-
-        $this->_classTokenReflection->setValue($this, null);
+        $classToken = null;
         foreach ($tokens[$stackPtr]['conditions'] as $condPtr => $condition) {
             if ($condition === T_CLASS || $condition === T_INTERFACE) {
-                $this->_classTokenReflection->setValue($this, $condPtr);
+                $classToken = $condPtr;
                 break;
             }
         }
@@ -157,7 +96,6 @@ class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs
         // Find the first doc comment.
         $commentStart  = ($phpcsFile->findPrevious(T_DOC_COMMENT, ($commentEnd - 1), null, true) + 1);
         $commentString = $phpcsFile->getTokensAsString($commentStart, ($commentEnd - $commentStart + 1));
-        $this->_methodNameReflection->setValue($this, $phpcsFile->getDeclarationName($stackPtr));
 
         try {
             $this->commentParser = new PHP_CodeSniffer_CommentParser_FunctionCommentParser($commentString, $phpcsFile);
@@ -169,161 +107,9 @@ class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs
         }
 
         $comment = $this->commentParser->getComment();
-        if ($comment === null) {
-            $error = 'Function doc comment is empty';
-            $phpcsFile->addError($error, $commentStart, 'Empty');
+        if (is_null($comment) === true) {
             return;
         }
-
-        // The first line of the comment should just be the /** code.
-        $eolPos    = strpos($commentString, $phpcsFile->eolChar);
-        $firstLine = substr($commentString, 0, $eolPos);
-        if ($firstLine !== '/**') {
-            $error = 'The open comment tag must be the only content on the line';
-            $phpcsFile->addError($error, $commentStart, 'ContentAfterOpen');
-        }
-
-        $this->processParams($commentStart, $commentEnd);
-        $this->processSees($commentStart);
-        $this->processReturn($commentStart, $commentEnd);
-        $this->processThrows($commentStart);
-
-        // Check for a comment description.
-        $short = $comment->getShortComment();
-        if (trim($short) === '') {
-            $error = 'Missing short description in function doc comment';
-            $phpcsFile->addError($error, $commentStart, 'MissingShort');
-            return;
-        }
-
-        // No extra newline before short description.
-        $newlineSpan = strspn($short, $phpcsFile->eolChar);
-        if ($short !== '' && $newlineSpan > 0) {
-            $error = 'Extra newline(s) found before function comment short description';
-            $phpcsFile->addError($error, ($commentStart + 1), 'SpacingBeforeShort');
-        }
-
-        $newlineCount = (substr_count($short, $phpcsFile->eolChar) + 1);
-
-        // Exactly one blank line between short and long description.
-        $long = $comment->getLongComment();
-        if (empty($long) === false) {
-            $between        = $comment->getWhiteSpaceBetween();
-            $newlineBetween = substr_count($between, $phpcsFile->eolChar);
-            if ($newlineBetween !== 2) {
-                $error = 'There must be exactly one blank line between descriptions in function comment';
-                $phpcsFile->addError($error, ($commentStart + $newlineCount + 1), 'SpacingBetween');
-            }
-
-            $newlineCount += $newlineBetween;
-
-            $testLong = trim($long);
-            if (preg_match('|\p{Lu}|u', $testLong[0]) === 0) {
-                $error = 'Function comment long description must start with a capital letter';
-                $phpcsFile->addError($error, ($commentStart + $newlineCount), 'LongNotCapital');
-            }
-        }//end if
-
-        // Exactly one blank line before tags.
-        $params = $this->commentParser->getTagOrders();
-        if (count($params) > 1) {
-            $newlineSpan = $comment->getNewlineAfter();
-            if ($newlineSpan !== 2) {
-                $error = 'There must be exactly one blank line before the tags in function comment';
-                if ($long !== '') {
-                    $newlineCount += (substr_count($long, $phpcsFile->eolChar) - $newlineSpan + 1);
-                }
-
-                $phpcsFile->addError($error, ($commentStart + $newlineCount), 'SpacingBeforeTags');
-                $short = rtrim($short, $phpcsFile->eolChar.' ');
-            }
-        }
-
-        // Short description must be single line and end with a full stop.
-        $testShort = trim($short);
-        $lastChar  = $testShort[(strlen($testShort) - 1)];
-        if (substr_count($testShort, $phpcsFile->eolChar) !== 0) {
-            $error = 'Function comment short description must be on a single line';
-            $phpcsFile->addError($error, ($commentStart + 1), 'ShortSingleLine');
-        }
-
-        $isEvent = $this->isEvent();
-
-        if ($isEvent === true && preg_match('/(\p{Lu}|\[)/u', $testShort[0]) === 0) {
-            $error = 'Event comment short description must start with a capital letter or an [';
-            $phpcsFile->addError($error, ($commentStart + 1), 'ShortNotCapital');
-        } else if ($isEvent === false && preg_match('/\p{Lu}/u', $testShort[0]) === 0) {
-            $error = 'Function comment short description must start with a capital letter';
-            $phpcsFile->addError($error, ($commentStart + 1), 'ShortNotCapital');
-        }
-
-        if ($lastChar !== '.') {
-            $error = 'Function comment short description must end with a full stop';
-            $phpcsFile->addError($error, ($commentStart + 1), 'ShortFullStop');
-        }
-
-        // Check for unknown/deprecated tags.
-        $this->processUnknownTags($commentStart, $commentEnd);
-
-        // The last content should be a newline and the content before
-        // that should not be blank. If there is more blank space
-        // then they have additional blank lines at the end of the comment.
-        $words   = $this->commentParser->getWords();
-        $lastPos = (count($words) - 1);
-        if (trim($words[($lastPos - 1)]) !== ''
-            || strpos($words[($lastPos - 1)], $this->currentFile->eolChar) === false
-            || trim($words[($lastPos - 2)]) === ''
-        ) {
-            $error = 'Additional blank lines found at end of function comment';
-            $this->currentFile->addError($error, $commentEnd, 'SpacingAfter');
-        }
-
-    }//end process()
-
-
-    /**
-     * Determines if a method is an event in the event handler class.
-     *
-     * @return bool
-     */
-    protected function isEvent()
-    {
-        if (substr($this->getClassName(), -12) === 'EventHandler') {
-            return substr($this->_methodNameReflection->getValue($this), 0, 2) == 'On';
-        }
-
-        return false;
-
-    }//end isEvent()
-
-
-    /**
-     * Returns class name to which current method belongs.
-     *
-     * @return string
-     */
-    protected function getClassName()
-    {
-        if ($this->_classTokenReflection->getValue($this) !== null) {
-            return $this->currentFile->getDeclarationName($this->_classTokenReflection->getValue($this));
-        }
-
-        return '';
-
-    }//end getClassName()
-
-
-    /**
-     * Process the return comment of this function comment.
-     *
-     * @param int $commentStart The position in the stack where the comment started.
-     * @param int $commentEnd   The position in the stack where the comment ended.
-     *
-     * @return void
-     */
-    protected function processReturn($commentStart, $commentEnd)
-    {
-        parent::processReturn($commentStart, $commentEnd);
 
         $return = $this->commentParser->getReturn();
 
@@ -334,11 +120,35 @@ class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs
             if ($returnType === '$this') {
                 $error = 'Function return type "%s" is invalid, please use "static" or "self" instead';
                 $data  = array($returnType);
-                $this->currentFile->addError($error, $errorPos, 'InvalidReturn', $data);
+                $phpcsFile->addError($error, $errorPos, 'InvalidThisReturn', $data);
             }
         }
 
-    }//end processReturn()
+        $short = trim($comment->getShortComment());
+        if ($short === '') {
+            return;
+        }
+
+        $isEvent = false;
+        if ($classToken !== null) {
+            $className = $phpcsFile->getDeclarationName($classToken);
+            if (substr($className, -12) === 'EventHandler') {
+                $methodName = $phpcsFile->getDeclarationName($stackPtr);
+                if (substr($methodName, 0, 2) == 'On') {
+                    $isEvent = true;
+                }
+            }
+        }
+
+        if ($isEvent === true && preg_match('/(\p{Lu}|\[)/u', $short[0]) === 0) {
+            $error = 'Event comment short description must start with a capital letter or an [';
+            $phpcsFile->addError($error, ($commentStart + 1), 'EventShortNotCapital');
+        } else if ($isEvent === false && preg_match('/\p{Lu}/u', $short[0]) === 0) {
+            $error = 'Function comment short description must start with a capital letter';
+            $phpcsFile->addError($error, ($commentStart + 1), 'NonEventShortNotCapital');
+        }
+
+    }//end process()
 
 
 }//end class
