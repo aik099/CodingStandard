10e60ebc21ae3574f1f699e94efa3404c8be39ec
Greg Sherwood <gsherwood@squiz.net>
2014-08-18 16:29:57 +1000

    Sniffs are now compatible with versions 1.x and 2.x

--- CodingStandard/Sniffs/Commenting/FunctionCommentSniff.php
+++ CodingStandard/Sniffs/Commenting/FunctionCommentSniff.php
@@ -46,6 +46,88 @@ class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
         parent::process($phpcsFile, $stackPtr);
+
+        if (isset($phpcsFile->fixer) === true) {
+            $this->processV2($phpcsFile, $stackPtr);
+        } else {
+            $this->processV1($phpcsFile, $stackPtr);
+        }
+
+    }//end process()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * Only works with 2.x versions of PHP_CodeSniffer.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function processV2(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $find   = PHP_CodeSniffer_Tokens::$methodPrefixes;
+        $find[] = T_WHITESPACE;
+
+        $commentEnd = $phpcsFile->findPrevious($find, ($stackPtr - 1), null, true);
+        if ($tokens[$commentEnd]['code'] !== T_DOC_COMMENT_CLOSE_TAG) {
+            return;
+        }
+
+        // Run the generic doc processing code.
+        $commentStart = $tokens[$commentEnd]['comment_opener'];
+        $sniff        = new Generic_Sniffs_Commenting_DocCommentSniff();
+        $sniff->process($phpcsFile, $commentStart);
+
+        $return = null;
+        foreach ($tokens[$commentStart]['comment_tags'] as $tag) {
+            if ($tokens[$tag]['content'] === '@return') {
+                $return = $tag;
+                break;
+            }
+        }
+
+        if ($return !== null) {
+            $returnType = $tokens[($return + 2)]['content'];
+            if ($returnType === '$this') {
+                $error = 'Function return type "%s" is invalid, please use "static" or "self" instead';
+                $data  = array($returnType);
+                $phpcsFile->addError($error, $return, 'InvalidThisReturn', $data);
+            }
+        }
+
+        $empty = array(
+                  T_DOC_COMMENT_WHITESPACE,
+                  T_DOC_COMMENT_STAR,
+                 );
+
+        $short = $phpcsFile->findNext($empty, ($commentStart + 1), $commentEnd, true);
+        if ($short === false || $tokens[$short]['code'] !== T_DOC_COMMENT_STRING) {
+            return;
+        }
+
+        $this->checkShort($phpcsFile, $stackPtr, $tokens[$short]['content'], $short);
+
+    }//end processV2()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * Only works with 1.x versions of PHP_CodeSniffer.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function processV1(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
         if ($this->commentParser === null) {
             return;
         }
@@ -55,8 +137,6 @@ class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs
             $this->commentParser = null;
             return;
         }
-
-        $tokens = $phpcsFile->getTokens();
 
         $find = array(
                  T_COMMENT,
@@ -68,14 +148,6 @@ class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs
 
         $commentEnd   = $phpcsFile->findPrevious($find, ($stackPtr - 1));
         $commentStart = ($phpcsFile->findPrevious(T_DOC_COMMENT, ($commentEnd - 1), null, true) + 1);
-
-        $classToken = null;
-        foreach ($tokens[$stackPtr]['conditions'] as $condPtr => $condition) {
-            if ($condition === T_CLASS || $condition === T_INTERFACE) {
-                $classToken = $condPtr;
-                break;
-            }
-        }
 
         $return = $this->commentParser->getReturn();
         if ($return !== null) {
@@ -95,6 +167,35 @@ class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs
             return;
         }
 
+        $this->checkShort($phpcsFile, $stackPtr, $short, ($commentStart + 1));
+        $this->commentParser = null;
+
+    }//end processV1()
+
+
+    /**
+     * Process the short description of a function comment.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the function token
+     *                                        in the stack passed in $tokens.
+     * @param string               $short     The content of the short description.
+     * @param int                  $errorPos  The position where an error should be thrown.
+     *
+     * @return void
+     */
+    public function checkShort(PHP_CodeSniffer_File $phpcsFile, $stackPtr, $short, $errorPos)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        $classToken = null;
+        foreach ($tokens[$stackPtr]['conditions'] as $condPtr => $condition) {
+            if ($condition === T_CLASS || $condition === T_INTERFACE) {
+                $classToken = $condPtr;
+                break;
+            }
+        }
+
         $isEvent = false;
         if ($classToken !== null) {
             $className = $phpcsFile->getDeclarationName($classToken);
@@ -108,15 +209,13 @@ class CodingStandard_Sniffs_Commenting_FunctionCommentSniff extends Squiz_Sniffs
 
         if ($isEvent === true && preg_match('/(\p{Lu}|\[)/u', $short[0]) === 0) {
             $error = 'Event comment short description must start with a capital letter or an [';
-            $phpcsFile->addError($error, ($commentStart + 1), 'EventShortNotCapital');
+            $phpcsFile->addError($error, $errorPos, 'EventShortNotCapital');
         } else if ($isEvent === false && preg_match('/\p{Lu}/u', $short[0]) === 0) {
             $error = 'Function comment short description must start with a capital letter';
-            $phpcsFile->addError($error, ($commentStart + 1), 'NonEventShortNotCapital');
+            $phpcsFile->addError($error, $errorPos, 'NonEventShortNotCapital');
         }
 
-        $this->commentParser = null;
-
-    }//end process()
+    }//end checkShort()
 
 
 }//end class
