d83a647bae09a6d6efc5b5940dee95f76d76ced6
Alexander Obuhovich <aik.bold@gmail.com>
2014-10-12 22:01:44 +0300

    The "phpcs-validate.sh" now can create "*.fixed" files when report is set to "diff"

--- phpcs-validate.sh
+++ phpcs-validate.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 if [ $# -lt 1 ]; then
-	echo -e "Usage: $0 <sniff.name>"
+	echo -e "Usage: $0 <sniff.name> <report-name>"
 	exit 1;
 fi
 
@@ -8,6 +8,25 @@ CS="CodingStandard"
 SNIFF=${1#$CS.}
 TEST_DATA_FILE="$CS/Tests/${SNIFF/.//}UnitTest.*inc"
 REPORT="${2-full}"
+PATCH_FILE="tmp.patch"
 
-echo -e "Report: $REPORT"
-vendor/bin/phpcs -vs --report=$REPORT --report-width=120 --standard=$CS --sniffs=$CS.$SNIFF $TEST_DATA_FILE
+if [ $REPORT == "diff" ]; then
+	echo "1. creating patch '${PATCH_FILE}' ..."
+	vendor/bin/phpcs --report=$REPORT --standard=$CS --sniffs=$CS.$SNIFF $TEST_DATA_FILE > $PATCH_FILE
+	
+	echo "2. applying patch '${PATCH_FILE}' ..."
+	patch -p0 -ui $PATCH_FILE
+
+	FILES=`ls $TEST_DATA_FILE`
+
+	echo "3. creating fixed files for each fixture file"
+	for FILE in $FILES; do
+		cp -f $FILE $FILE.fixed
+	done
+
+	patch -p0 -R -ui $PATCH_FILE
+else
+	echo -e "Report: $REPORT"
+	vendor/bin/phpcs -vs --report=$REPORT --report-width=120 --standard=$CS --sniffs=$CS.$SNIFF $TEST_DATA_FILE
+fi
+
