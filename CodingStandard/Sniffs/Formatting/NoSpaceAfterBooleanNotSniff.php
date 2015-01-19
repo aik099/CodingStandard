65628c7a93ceb31e3644f30786d34e9f876eaa10
Alexander Obuhovich <aik.bold@gmail.com>
2015-01-19 22:42:21 +0200

    Adding "NoSpaceAfterBooleanNot" sniff
    Closes #55

---
+++ CodingStandard/Sniffs/Formatting/NoSpaceAfterBooleanNotSniff.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * CodingStandard_Sniffs_Formatting_NoSpaceAfterBooleanNotSniff.
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+
+/**
+ * CodingStandard_Sniffs_Formatting_NoSpaceAfterBooleanNotSniff.
+ *
+ * Ensures there is no space after boolean not operator.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+class CodingStandard_Sniffs_Formatting_NoSpaceAfterBooleanNotSniff implements PHP_CodeSniffer_Sniff
+{
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_BOOLEAN_NOT);
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        if ($tokens[($stackPtr + 1)]['code'] !== T_WHITESPACE) {
+            return;
+        }
+
+        $error = 'A boolean not operator must not be followed by a space';
+        $fix   = $phpcsFile->addFixableError($error, $stackPtr, 'SpaceFound');
+        if ($fix === true) {
+            $phpcsFile->fixer->replaceToken(($stackPtr + 1), '');
+        }
+
+    }//end process()
+
+
+}//end class
---
+++ CodingStandard/Tests/Formatting/NoSpaceAfterBooleanNotUnitTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Unit test class for the NoSpaceAfterBooleanNot sniff.
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+
+/**
+ * Unit test class for the NoSpaceAfterBooleanNot sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+class CodingStandard_Tests_Formatting_NoSpaceAfterBooleanNotUnitTest extends AbstractSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @param string $testFile Name of the file with test data.
+     *
+     * @return array <int, int>
+     */
+    public function getErrorList($testFile)
+    {
+        return array(
+                11 => 1,
+                12 => 1,
+                13 => 2,
+               );
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @param string $testFile Name of the file with test data.
+     *
+     * @return array <int, int>
+     */
+    public function getWarningList($testFile)
+    {
+        return array();
+
+    }//end getWarningList()
+
+
+}//end class
+
+?>
