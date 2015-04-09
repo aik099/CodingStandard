b231d97130b35d7ec2124a2822618effebab86fe
Alexander Obuhovich <aik.bold@gmail.com>
2015-04-09 22:37:13 +0300

    Validate spacing around "->" (object operator)
    Closes #73

---
+++ CodingStandard/Sniffs/WhiteSpace/ObjectOperatorSpacingSniff.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * CodingStandard_Sniffs_WhiteSpace_ObjectOperatorSpacingSniff.
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Greg Sherwood <gsherwood@squiz.net>
+ * @author   Marc McIntyre <mmcintyre@squiz.net>
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+
+/**
+ * CodingStandard_Sniffs_WhiteSpace_ObjectOperatorSpacingSniff.
+ *
+ * Ensure there is no whitespace before/after a object operator.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Greg Sherwood <gsherwood@squiz.net>
+ * @author   Marc McIntyre <mmcintyre@squiz.net>
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+class CodingStandard_Sniffs_WhiteSpace_ObjectOperatorSpacingSniff implements PHP_CodeSniffer_Sniff
+{
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_OBJECT_OPERATOR);
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        if ($tokens[($stackPtr - 1)]['code'] === T_WHITESPACE
+            && $tokens[($stackPtr - 2)]['line'] === $tokens[$stackPtr]['line']
+        ) {
+            $error = 'Space found before object operator';
+            $fix = $phpcsFile->addFixableError($error, $stackPtr, 'Before');
+            if ($fix === true) {
+                $phpcsFile->fixer->replaceToken(($stackPtr - 1), '');
+            }
+        }
+
+        if ($tokens[($stackPtr + 1)]['code'] === T_WHITESPACE
+            && $tokens[($stackPtr + 2)]['line'] === $tokens[$stackPtr]['line']
+        ) {
+            $error = 'Space found after object operator';
+            $fix = $phpcsFile->addFixableError($error, $stackPtr, 'After');
+            if ($fix === true) {
+                $phpcsFile->fixer->replaceToken(($stackPtr + 1), '');
+            }
+        }
+
+    }//end process()
+
+
+}//end class
---
+++ CodingStandard/Tests/WhiteSpace/ObjectOperatorSpacingUnitTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Unit test class for the ObjectOperatorSpacing sniff.
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Greg Sherwood <gsherwood@squiz.net>
+ * @author   Marc McIntyre <mmcintyre@squiz.net>
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+
+/**
+ * Unit test class for the ObjectOperatorSpacing sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Greg Sherwood <gsherwood@squiz.net>
+ * @author   Marc McIntyre <mmcintyre@squiz.net>
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+class CodingStandard_Tests_WhiteSpace_ObjectOperatorSpacingUnitTest extends AbstractSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @param string $testFile Name of the file with test data.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList($testFile)
+    {
+        return array(
+                3 => 1,
+                4 => 2,
+                6 => 2,
+               );
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @param string $testFile Name of the file with test data.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList($testFile)
+    {
+        return array();
+
+    }//end getWarningList()
+
+
+}//end class
