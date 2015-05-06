85238f21eb8e77899e2643baef6a50382432ce03
Alexander Obuhovich <aik.bold@gmail.com>
2015-05-06 11:54:25 +0300

    Report assignments in if/elseif as an error
    Closes #8

---
+++ CodingStandard/Sniffs/ControlStructures/AssignmentInConditionSniff.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * CodingStandard_Sniffs_ControlStructures_AssignmentInConditionSniff.
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+
+/**
+ * Checks that assignments in conditions are not used.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+class CodingStandard_Sniffs_ControlStructures_AssignmentInConditionSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array('PHP');
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(
+                T_IF,
+                T_ELSEIF,
+               );
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile All the tokens found in the document.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        if (isset($tokens[$stackPtr]['parenthesis_opener']) === false) {
+            return;
+        }
+
+        $equalOperator = $phpcsFile->findNext(
+            T_EQUAL,
+            $tokens[$stackPtr]['parenthesis_opener'],
+            $tokens[$stackPtr]['parenthesis_closer']
+        );
+
+        if ($equalOperator === false) {
+            return;
+        }
+
+        $phpcsFile->addError(
+            'Assignment in "' . $tokens[$stackPtr]['content'] . '" control structure is forbidden',
+            $equalOperator,
+            'Forbidden'
+        );
+
+    }//end process()
+
+
+}//end class
---
+++ CodingStandard/Tests/ControlStructures/AssignmentInConditionUnitTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * CodingStandard_Tests_ControlStructures_AssignmentInConditionUnitTest.
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+
+/**
+ * Unit test class for the AssignmentInCondition sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @author   Alexander Obuhovich <aik.bold@gmail.com>
+ * @license  https://github.com/aik099/CodingStandard/blob/master/LICENSE BSD 3-Clause
+ * @link     https://github.com/aik099/CodingStandard
+ */
+class CodingStandard_Tests_ControlStructures_AssignmentInConditionUnitTest extends AbstractSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @param string $testFile Name of the file with test data.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList($testFile)
+    {
+        return array(
+                17 => 1,
+                19 => 1,
+                23 => 1,
+                25 => 1,
+                29 => 1,
+                31 => 1,
+               );
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @param string $testFile Name of the file with test data.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList($testFile)
+    {
+        return array();
+
+    }//end getWarningList()
+
+
+}//end class
