cdcb5456bdc1436dca76c5f69aa7923915236a64
George Mponos <gmponos@gmail.com>
2017-02-04 23:30:13 +0200

    Added also tests

--- CodingStandard/Tests/Classes/ClassCreateInstanceUnitTest.php
+++ CodingStandard/Tests/Classes/ClassCreateInstanceUnitTest.php
@@ -43,6 +43,10 @@ class CodingStandard_Tests_Classes_ClassCreateInstanceUnitTest extends AbstractS
                 // Space after class name and it's opening brace.
                 4 => 1,
                 5 => 1,
+                7 => 1,
+                8 => 1,
+                10 => 1,
+                11 => 1,
                );
 
     }//end getErrorList()
